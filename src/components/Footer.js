0566f38355610dd96a1e772a109cf25c26e416c6
Sarthak Verma <sarthakvdev@gmail.com>
2022-07-18 03:07:53 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,43 +5,44 @@ const Footer = () => {
   const navigation = [
     {
       name: "email us",
-      href: "mailto:buildspaceind@gmail.com",
+      href: "mailto:mainnetdao@gmail.com",
     },
     {
       name: "twitter",
-      href: "https://twitter.com/buildspaceind",
-    }, 
-       {
+      href: "https://twitter.com/mainnethh",
+    },
+    {
       name: "instagram",
-      href: "https://www.instagr.am/buildspaceIND",
+      href: "#",
     },
     {
       name: "discord",
-      href: "https://discord.com/invite/vPmqZqgpsS",
+      href: "#",
     },
   ];
 
   return (
-    <footer className="text-black">
+    <footer>
       <div className="max-w-7xl mx-auto py-12 px-6 sm:px-6 lg:px-8 md:flex md:items-center md:justify-between pb-20">
         <div className="mt-8 md:mt-0 md:order-1 text-xl">
-          <Image
+          {/* <Image
             className="w-14 h-14"
             src="/buildspace-logo.svg"
             alt="logo"
             height="56"
             width="56"
-          />
-          <p className="font-bold">buildspace India</p>
-          <p className="font-normal">where devs learn by building.</p>
+          /> */}
+          <p className="font-bold">mainnetDAO</p>
+          <p className="font-normal">where devs learn, buidl and ship together.</p>
           <p className="font-normal text-[18px] mt-5 text-gray-600">
-            Copyright © {new Date().getFullYear()} BuildSpace. All rights reserved.
+            Copyright © {new Date().getFullYear()} mainnetDAO. All rights
+            reserved.
           </p>
         </div>
         <div className="flex mt-8 md:mt-0 flex-col md:text-right md:justify-center md:space-x-8 md:order-2">
-          <p className="text-2xl font-black mb-6 hidden md:block">
+          {/* <p className="text-2xl font-black mb-6 hidden md:block">
             Contact with us
-          </p>
+          </p> */}
           <ul>
             {navigation.map((item) => (
               <li
